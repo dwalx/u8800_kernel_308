36fe41c029501a75a364cfb8be694476821cc5f0
dwalx <dwalx79@gmail.com>
2013-08-21 11:10:16 +0400

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 29 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#29 PREEMPT Wed Aug 21 11:01:52 MSK 2013"
+#define LINUX_COMPILE_BY "alx"
+#define LINUX_COMPILE_HOST "universe"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
